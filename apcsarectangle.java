d145840bfd343cb7fb1bc6e26c1e9bb0b9223c17
Stephen Mazza <smazza@ucvts.tec.nj.us>
2019-10-01 09:25:21 -0400

    Finished 52

---
+++ apcsarectangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class apcsarectangle {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.print("\nPlease enter the length of your rectangle: ");
+        double length = in.nextDouble();
+    System.out.print("\nPlease enter the width of your rectangle: ");
+        double width = in.nextDouble();
+
+        double area = length * width;
+
+        System.out.printf("\nThe area of a "
+        System.out.printdf("%.1f"length);
+        System.out.print("-by-")
+        System.out.printf("%.1f"width);
+        System.out.printf("rectangle is %.1f" area);
+        
+        in.close();
+      }
+}
